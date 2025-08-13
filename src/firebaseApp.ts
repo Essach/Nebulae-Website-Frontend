64c754fc309c9923812bea77654a5d74051db835
Adrian <adrian.wozny@interia.pl>
2025-08-13 11:33:01 +0200

    security

--- src/firebaseApp.ts
+++ src/firebaseApp.ts
@@ -4,13 +4,13 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBmdmGi1QWnTWiimqCrOaT4ZR-gi5xH1Vg",
-    authDomain: "nebulae-84df9.firebaseapp.com",
-    projectId: "nebulae-84df9",
-    storageBucket: "nebulae-84df9.firebasestorage.app",
-    messagingSenderId: "798461561621",
-    appId: "1:798461561621:web:840c6194672ec3bd64b71c",
-    measurementId: "G-279ZLKZL1R",
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID,
+    measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
