614679f5aac1dd2da9d69737540283fdf5e49849
Adrian <adrian.wozny@interia.pl>
2025-08-16 00:48:13 +0200

    feat. alert

--- src/components/RewardsShop/RewardsShop.tsx
+++ src/components/RewardsShop/RewardsShop.tsx
@@ -3,25 +3,32 @@ import nitro from "../../images/nitro.png";
 import nitroPremium from "../../images/nitroPremium.png";
 import woo from "../../images/woo.png";
 import "./RewardsShop.scss";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useUser } from "../../context/UserContext/UserContext";
 import request from "../../helpers/request.ts";
 import { auth } from "../../firebaseApp.ts";
+import { FaRegCheckSquare } from "react-icons/fa";
+import { MdOutlineCancel } from "react-icons/md";
+import { MdErrorOutline } from "react-icons/md";
 
 const RewardsShop = () => {
+    // const [a, setA] = useState<boolean>(false);
+    const [currentAlert, setCurrentAlert] = useState<string>("");
+    const [alertMessage, setAlertMessage] = useState<string>("");
+    const [alertOpen, setAlertOpen] = useState<boolean>(false);
+
     const { pathname } = useLocation();
 
     const userData = useUser();
     const uid = userData.uid;
 
-    const handleCheckBonus = async (bonusType: string) => {
-        const { data, status } = await request.get("/bonuses/check", {
+    const handleRedeemBonus = async (bonusType: string) => {
+        const { data, status } = await request.get("/bonuses/redeem", {
             params: {
                 userId: uid,
                 bonusType: bonusType,
             },
         });
-        console.log("hi");
         console.log(data, status);
     };
 
@@ -32,99 +39,165 @@ const RewardsShop = () => {
     return (
         <div>
             {auth.currentUser ? (
-                <div className="rewardsShop">
-                    <div className="earnPoints">
-                        <div className="earnPointsTop">
-                            <h1 className="earnPointsTitle">Earn points</h1>
-                            <div className="titleLine"></div>
+                <>
+                    <div className="rewardsShop">
+                        <div className="earnPoints">
+                            <div className="earnPointsTop">
+                                <h1 className="earnPointsTitle">Earn points</h1>
+                                <div className="titleLine"></div>
+                            </div>
+                            <div className="earnPointsShop">
+                                <div className="signUpBonus">
+                                    <div className="infoLabel">
+                                        <div className="labelText">
+                                            <h4 className="desc">
+                                                Sign up bonus{" "}
+                                                <span>{`(One time)`}</span>
+                                            </h4>
+                                            <div className="textLine"></div>
+                                            <p className="pointsAmount">
+                                                300 points
+                                            </p>
+                                        </div>
+                                    </div>
+                                    <button
+                                        className="redeem"
+                                        onClick={() =>
+                                            setAlertOpen((prev) => !prev)
+                                        }
+                                        // onClick={async () =>
+                                        //     await handleRedeemBonus("signUp")
+                                        // }
+                                    >
+                                        Redeem points
+                                    </button>
+                                </div>
+                                <div className="serverBoost">
+                                    <div className="infoLabel">
+                                        <div className="labelText">
+                                            <h4 className="desc">
+                                                Server boost{" "}
+                                                <span>{`(Every month)`}</span>
+                                            </h4>
+                                            <div className="textLine"></div>
+                                            <p className="pointsAmount">
+                                                200 points
+                                            </p>
+                                        </div>
+                                    </div>
+                                    <button
+                                        className="redeem"
+                                        onClick={async () =>
+                                            await handleRedeemBonus(
+                                                "serverBoost"
+                                            )
+                                        }
+                                    >
+                                        Redeem points
+                                    </button>
+                                </div>
+                                <div className="serverSub">
+                                    <div className="infoLabel">
+                                        <div className="labelText">
+                                            <h4 className="desc">
+                                                Server subscription{" "}
+                                                <span>{`(Every month)`}</span>
+                                            </h4>
+                                            <div className="textLine"></div>
+                                            <p className="pointsAmount">
+                                                400 points
+                                            </p>
+                                        </div>
+                                    </div>
+                                    <button
+                                        className="redeem"
+                                        onClick={async () =>
+                                            await handleRedeemBonus(
+                                                "serverSubscription"
+                                            )
+                                        }
+                                    >
+                                        Redeem points
+                                    </button>
+                                </div>
+                            </div>
                         </div>
-                        <div className="earnPointsShop">
-                            <div className="signUpBonus">
-                                <div className="infoLabel">
-                                    <div className="labelText">
-                                        <h4 className="desc">Sign up bonus</h4>
-                                        <div className="textLine"></div>
-                                        <p className="pointsAmount">
-                                            300 points
-                                        </p>
-                                    </div>
-                                </div>
-                                <button
-                                    className="check"
-                                    onClick={async () =>
-                                        await handleCheckBonus("signUp")
-                                    }
-                                >
-                                    Check for availability
-                                </button>
+                        <div className="pointsShop">
+                            <div className="pointsShopTop">
+                                <h1 className="pointsShopTitle">
+                                    Points shop{" "}
+                                </h1>
+                                <div className="titleLine"></div>
                             </div>
-                            <div className="serverBoost">
-                                <div className="infoLabel">
-                                    <div className="labelText">
-                                        <h4 className="desc">Server boost</h4>
-                                        <div className="textLine"></div>
-                                        <p className="pointsAmount">
-                                            200 points
-                                        </p>
+                            <div className="shopButtons">
+                                <button className="nitroBasic">
+                                    <img src={nitro} alt="" />
+                                    <div className="rewardButtonText">
+                                        <h3>Discord nitro basic</h3>
+                                        <div className="buttonLine"></div>
+                                        <p>2,000 Points</p>
                                     </div>
-                                </div>
-                                <button className="check">
-                                    Check for availability
-                                    <span>{`(available every month)`}</span>
                                 </button>
-                            </div>
-                            <div className="serverSub">
-                                <div className="infoLabel">
-                                    <div className="labelText">
-                                        <h4 className="desc">
-                                            Server subscription
-                                        </h4>
-                                        <div className="textLine"></div>
-                                        <p className="pointsAmount">
-                                            400 points
-                                        </p>
+                                <button className="nitroPremium">
+                                    <img src={nitroPremium} alt="" />
+                                    <div className="rewardButtonText">
+                                        <h3>Discord nitro premium</h3>
+                                        <div className="buttonLine"></div>
+                                        <p>3,000 Points</p>
                                     </div>
-                                </div>
-                                <button className="check">
-                                    Check for availability
-                                    <span>{`(available every month)`}</span>
+                                </button>
+                                <button className="customRole">
+                                    <img src={woo} alt="" />
+                                    <div className="rewardButtonText">
+                                        <h3>Custom server role</h3>
+                                        <div className="buttonLine"></div>
+                                        <p>1,000 Points</p>
+                                    </div>
                                 </button>
                             </div>
                         </div>
                     </div>
-                    <div className="pointsShop">
-                        <div className="pointsShopTop">
-                            <h1 className="pointsShopTitle">Points shop </h1>
-                            <div className="titleLine"></div>
-                        </div>
-                        <div className="shopButtons">
-                            <button className="nitroBasic">
-                                <img src={nitro} alt="" />
-                                <div className="rewardButtonText">
-                                    <h3>Discord nitro basic</h3>
-                                    <div className="buttonLine"></div>
-                                    <p>2,000 Points</p>
-                                </div>
-                            </button>
-                            <button className="nitroPremium">
-                                <img src={nitroPremium} alt="" />
-                                <div className="rewardButtonText">
-                                    <h3>Discord nitro premium</h3>
-                                    <div className="buttonLine"></div>
-                                    <p>3,000 Points</p>
-                                </div>
-                            </button>
-                            <button className="customRole">
-                                <img src={woo} alt="" />
-                                <div className="rewardButtonText">
-                                    <h3>Custom server role</h3>
-                                    <div className="buttonLine"></div>
-                                    <p>1,000 Points</p>
-                                </div>
-                            </button>
-                        </div>
+                    <div
+                        className={`rewardsAlert ${
+                            alertOpen ? "alertVisible" : "alertHidden"
+                        }`}
+                    >
+                        {currentAlert == "success" ? (
+                            <FaRegCheckSquare
+                                size={30}
+                                fill="green"
+                                filter="drop-shadow(0px 0px 2px green)"
+                            />
+                        ) : (
+                            ""
+                        )}
+                        {currentAlert == "wrong" ? (
+                            <MdOutlineCancel
+                                size={30}
+                                fill="red"
+                                filter="drop-shadow(0px 0px 2px red)"
+                            />
+                        ) : (
+                            ""
+                        )}
+                        {currentAlert == "error" ? (
+                            <MdErrorOutline
+                                size={30}
+                                fill="orange"
+                                filter="drop-shadow(0px 0px 2px orange)"
+                            />
+                        ) : (
+                            ""
+                        )}
+                        <MdErrorOutline
+                            size={30}
+                            fill="orange"
+                            filter="drop-shadow(0px 0px 2px orange)"
+                        />
+                        <p>You are not eligible to redeem these points.</p>
+                        {/* <p>{alertMessage}</p> */}
                     </div>
-                </div>
+                </>
             ) : (
                 <div className="noUserRewards">
                     Log in to see the rewards shop!
