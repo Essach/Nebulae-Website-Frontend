a163067ab542826ae450fc78e0ecc662280d06ab
Adrian <adrian.wozny@interia.pl>
2025-08-15 14:15:40 +0200

    es

--- src/components/RewardsShop/RewardsShop.tsx
+++ src/components/RewardsShop/RewardsShop.tsx
@@ -6,6 +6,7 @@ import "./RewardsShop.scss";
 import { useEffect } from "react";
 import { useUser } from "../../context/UserContext/UserContext";
 import request from "../../helpers/request.ts";
+import { auth } from "../../firebaseApp.ts";
 
 const RewardsShop = () => {
     const { pathname } = useLocation();
@@ -29,90 +30,106 @@ const RewardsShop = () => {
     }, [pathname]);
 
     return (
-        <div className="rewardsShop">
-            <div className="earnPoints">
-                <div className="earnPointsTop">
-                    <h1 className="earnPointsTitle">Earn points</h1>
-                    <div className="titleLine"></div>
-                </div>
-                <div className="earnPointsShop">
-                    <div className="signUpBonus">
-                        <div className="infoLabel">
-                            <div className="labelText">
-                                <h4 className="desc">Sign up bonus</h4>
-                                <div className="textLine"></div>
-                                <p className="pointsAmount">300 points</p>
+        <div>
+            {auth.currentUser ? (
+                <div className="rewardsShop">
+                    <div className="earnPoints">
+                        <div className="earnPointsTop">
+                            <h1 className="earnPointsTitle">Earn points</h1>
+                            <div className="titleLine"></div>
+                        </div>
+                        <div className="earnPointsShop">
+                            <div className="signUpBonus">
+                                <div className="infoLabel">
+                                    <div className="labelText">
+                                        <h4 className="desc">Sign up bonus</h4>
+                                        <div className="textLine"></div>
+                                        <p className="pointsAmount">
+                                            300 points
+                                        </p>
+                                    </div>
+                                </div>
+                                <button
+                                    className="check"
+                                    onClick={async () =>
+                                        await handleCheckBonus("signUp")
+                                    }
+                                >
+                                    Check for availability
+                                </button>
+                            </div>
+                            <div className="serverBoost">
+                                <div className="infoLabel">
+                                    <div className="labelText">
+                                        <h4 className="desc">Server boost</h4>
+                                        <div className="textLine"></div>
+                                        <p className="pointsAmount">
+                                            200 points
+                                        </p>
+                                    </div>
+                                </div>
+                                <button className="check">
+                                    Check for availability
+                                    <span>{`(available every month)`}</span>
+                                </button>
+                            </div>
+                            <div className="serverSub">
+                                <div className="infoLabel">
+                                    <div className="labelText">
+                                        <h4 className="desc">
+                                            Server subscription
+                                        </h4>
+                                        <div className="textLine"></div>
+                                        <p className="pointsAmount">
+                                            400 points
+                                        </p>
+                                    </div>
+                                </div>
+                                <button className="check">
+                                    Check for availability
+                                    <span>{`(available every month)`}</span>
+                                </button>
                             </div>
                         </div>
-                        <button
-                            className="check"
-                            onClick={async () =>
-                                await handleCheckBonus("signUp")
-                            }
-                        >
-                            Check for availability
-                        </button>
                     </div>
-                    <div className="serverBoost">
-                        <div className="infoLabel">
-                            <div className="labelText">
-                                <h4 className="desc">Server boost</h4>
-                                <div className="textLine"></div>
-                                <p className="pointsAmount">200 points</p>
-                            </div>
+                    <div className="pointsShop">
+                        <div className="pointsShopTop">
+                            <h1 className="pointsShopTitle">Points shop </h1>
+                            <div className="titleLine"></div>
                         </div>
-                        <button className="check">
-                            Check for availability
-                            <span>{`(available every month)`}</span>
-                        </button>
-                    </div>
-                    <div className="serverSub">
-                        <div className="infoLabel">
-                            <div className="labelText">
-                                <h4 className="desc">Server subscription</h4>
-                                <div className="textLine"></div>
-                                <p className="pointsAmount">400 points</p>
-                            </div>
+                        <div className="shopButtons">
+                            <button className="nitroBasic">
+                                <img src={nitro} alt="" />
+                                <div className="rewardButtonText">
+                                    <h3>Discord nitro basic</h3>
+                                    <div className="buttonLine"></div>
+                                    <p>2,000 Points</p>
+                                </div>
+                            </button>
+                            <button className="nitroPremium">
+                                <img src={nitroPremium} alt="" />
+                                <div className="rewardButtonText">
+                                    <h3>Discord nitro premium</h3>
+                                    <div className="buttonLine"></div>
+                                    <p>3,000 Points</p>
+                                </div>
+                            </button>
+                            <button className="customRole">
+                                <img src={woo} alt="" />
+                                <div className="rewardButtonText">
+                                    <h3>Custom server role</h3>
+                                    <div className="buttonLine"></div>
+                                    <p>1,000 Points</p>
+                                </div>
+                            </button>
                         </div>
-                        <button className="check">
-                            Check for availability
-                            <span>{`(available every month)`}</span>
-                        </button>
                     </div>
                 </div>
-            </div>
-            <div className="pointsShop">
-                <div className="pointsShopTop">
-                    <h1 className="pointsShopTitle">Points shop </h1>
-                    <div className="titleLine"></div>
+            ) : (
+                <div className="noUserRewards">
+                    Log in to see the rewards shop!
                 </div>
-                <div className="shopButtons">
-                    <button className="nitroBasic">
-                        <img src={nitro} alt="" />
-                        <div className="rewardButtonText">
-                            <h3>Discord nitro basic</h3>
-                            <div className="buttonLine"></div>
-                            <p>2,000 Points</p>
-                        </div>
-                    </button>
-                    <button className="nitroPremium">
-                        <img src={nitroPremium} alt="" />
-                        <div className="rewardButtonText">
-                            <h3>Discord nitro premium</h3>
-                            <div className="buttonLine"></div>
-                            <p>3,000 Points</p>
-                        </div>
-                    </button>
-                    <button className="customRole">
-                        <img src={woo} alt="" />
-                        <div className="rewardButtonText">
-                            <h3>Custom server role</h3>
-                            <div className="buttonLine"></div>
-                            <p>1,000 Points</p>
-                        </div>
-                    </button>
-                </div>
-            </div>
+            )}
         </div>
     );
 };
--- src/discord/logout.tsx
+++ src/discord/logout.tsx
@@ -1,12 +1,19 @@
 import { signOut } from "firebase/auth";
 import { auth } from "../firebaseApp";
 import { changeUsername } from "../helpers/localStorage";
+import { useUser } from "../context/UserContext/UserContext";
 
 function LogoutButton() {
+    const userData = useUser();
+    const setUid = userData.setUid;
+    const setUsername = userData.setUsername;
+
     const handleSignOut = async () => {
         try {
             await signOut(auth);
             changeUsername("");
+            setUid("");
+            setUsername("");
             console.log("Signed out successfully");
         } catch (error) {
             console.error("Error signing out:", error);
